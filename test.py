ceb2e012c0228cc95e19c1e2321b7c11f9f2ee6e
FrancoConvertini <Franco.convertini@gmail.com>
2024-05-24 20:17:31 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hola Mundo")
+import cv2
+import mediapipe as mp 
+print("OpenCV version:", cv2.__version__)
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imshow('Raw Webcam Feed', frame)
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
